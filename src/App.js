3bb244ea952104ab2e5e0dcb3114b8a5cf8f6ff2
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-07-16 15:15:48 +0530

    rendering items list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import './index.css';
 
+const initialItems = [
+  {
+    id: 1,
+    description: "Passports",
+    quantity: 2,
+    packed: false
+  },
+  {
+    id: 2,
+    description: "Socks",
+    quantity: 12,
+    packed: false
+  },
+  {
+    id: 3,
+    description: "Charger",
+    quantity: 1,
+    packed: false
+  }
+];
+
 export default function App() {
   return (
     <div className='app'>
@@ -30,9 +51,24 @@ function Form(){
 function PackingList(){
   return (
     <div className='list'>
-      Packing List
+      <ul>
+        {
+        initialItems.map(item => <Item item={item} />)
+        }
+      </ul>
     </div>
   );
+}
+
+function Item({item}){
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration: 'line-through'} : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  )
 }
 
 function Stats(){
