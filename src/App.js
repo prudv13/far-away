f68cbafde9f0ef4cca6f8d09df46bae3a8e3ad5e
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-07-16 17:42:11 +0530

    building form and handling submit

--- src/App.js
+++ src/App.js
@@ -41,10 +41,28 @@ function Logo(){
 }
 
 function Form(){
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <div className='add-form'>
+    <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip ?</h3>
-    </div>
+      <select>
+      {
+        Array.from({length: 20}, (_, i) => i+1)
+        .map(num => 
+          <option value={num} key={num}>
+            {num}
+          </option>
+        )
+      }
+      </select>
+      <input type='text' placeholder='Item...' />
+      <button>ADD</button>
+    </form>
   );
 }
 
@@ -53,7 +71,7 @@ function PackingList(){
     <div className='list'>
       <ul>
         {
-        initialItems.map(item => <Item item={item} />)
+        initialItems.map(item => <Item item={item} key={item.id} />)
         }
       </ul>
     </div>
