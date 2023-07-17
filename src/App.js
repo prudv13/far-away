5fe3a697bbc6977c6ea6c0f1f35da2ed11ed7bb5
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-07-17 14:31:14 +0530

    update item - complex immutable data operation

--- src/App.js
+++ src/App.js
@@ -13,11 +13,22 @@ export default function App() {
     setItems(items => items.filter(item=> item.id !== id));
   }
 
+  function handleToggleItem(id){
+    setItems(items => items.map(
+      item => item.id === id ? {...item, packed: !item.packed} : item
+      )
+    )
+  }
+
   return (
     <div className='app'>
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} onDeleteItem={handleDeleteItem} />
+      <PackingList 
+        items={items} 
+        onDeleteItem={handleDeleteItem} 
+        onToggleItem = {handleToggleItem} 
+      />
       <Stats />
     </div>
   );
@@ -75,21 +86,31 @@ function Form({onAddItems}){
   );
 }
 
-function PackingList({items, onDeleteItem}){
+function PackingList({items, onDeleteItem, onToggleItem}){
   return (
     <div className='list'>
       <ul>
         {
-        items.map(item => <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />)
+        items.map(item => <Item 
+          item={item} 
+          key={item.id} 
+          onDeleteItem={onDeleteItem}
+          onToggleItem={onToggleItem}
+        />)
         }
       </ul>
     </div>
   );
 }
 
-function Item({item, onDeleteItem}){
+function Item({item, onDeleteItem, onToggleItem}){
   return (
     <li>
+      <input 
+        type='checkbox' 
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
       <span style={item.packed ? {textDecoration: 'line-through'} : {}}>
         {item.quantity} {item.description}
       </span>
