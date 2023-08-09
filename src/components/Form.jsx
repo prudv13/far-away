7c7841cd2427df39b014558f6d0ac2201356def3
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-08-09 18:05:58 +0530

    modified quantity input and item list styling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,35 +3,40 @@ import { useState } from "react";
 export default function Form({onAddItems}){
 
     const [description, setDescription] = useState("");
-    const [quantity, setQuantity] = useState(1);
-  
+    const [quantity, setQuantity] = useState("");
+
     function handleSubmit(e){
       e.preventDefault();
       if(!description) return;
-      const newItem = { 
-        description, 
-        quantity, 
-        packed: false, 
-        id: Date.now()
-      };
-      onAddItems(newItem);
-      setDescription("");
-      setQuantity(1);
+      if(quantity <= 0){
+        alert("Please enter appropriate quantity!");
+        setDescription("");
+        setQuantity("");
+      }
+      else {
+        const newItem = { 
+          description, 
+          quantity, 
+          packed: false, 
+          id: Date.now()
+        };
+        console.log(typeof Number("-1"));
+        onAddItems(newItem);
+        setDescription("");
+        setQuantity("");
+      }
     }
   
     return (
       <form className='add-form' onSubmit={handleSubmit}>
         <h3>What do you need for your 😍 trip ?</h3>
-        <select value={quantity} onChange={e=>setQuantity(Number(e.target.value))}>
-        {
-          Array.from({length: 20}, (_, i) => i+1)
-          .map(num => 
-            <option value={num} key={num}>
-              {num}
-            </option>
-          )
-        }
-        </select>
+        <input 
+          type='text' 
+          placeholder='Quantity' 
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+          style={{width: "150px"}}
+        />
         <input 
           type='text' 
           placeholder='Item...' 
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -9,7 +9,9 @@ export default function Item({item, onDeleteItem, onToggleItem}){
         <span style={item.packed ? {textDecoration: 'line-through'} : {}}>
           {item.quantity} {item.description}
         </span>
-        <button onClick={()=>onDeleteItem(item.id)}>❌</button>
+        <div className="delete-container" onClick={()=>onDeleteItem(item.id)}>
+          <i class="uil uil-trash-alt delete-icon"></i>
+        </div>
       </li>
     )
 }
